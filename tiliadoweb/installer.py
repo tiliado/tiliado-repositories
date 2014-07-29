1ad9ec1f816b0c0e2938f2aeec71896fcaa46dc1
Jiří Janoušek <janousek.jiri@gmail.com>
2014-07-29 13:15:13 +0200

    Remember username and token
    Signed-off-by: Jiří Janoušek <janousek.jiri@gmail.com>

--- tiliadoweb/installer.py
+++ tiliadoweb/installer.py
@@ -1,10 +1,14 @@
+import os
+import json
 from gi.repository import Gtk
 from tiliadoweb.api import ApiError
 
+CONFIG_FILENAME = "config.json"
 class Installer:
-    def __init__(self, api, stack, login_page, repositories_page):
+    def __init__(self, api, config_dir, stack, login_page, repositories_page):
         self.api = api
         self.stack = stack
+        self.config_dir = config_dir
         
         self.login_page = login_page
         stack.add(login_page)
@@ -15,6 +19,24 @@ class Installer:
         stack.add(repositories_page)
         repositories_page.back_button.connect("clicked", self.on_quit_clicked)
         repositories_page.ok_button.connect("clicked", self.on_quit_clicked)
+        
+        try:
+            with open(os.path.join(config_dir, CONFIG_FILENAME), "rt") as f:
+                self.config = json.load(f)
+        except OSError as e:
+            self.config = {}
+        except ValueError as e:
+            print(e)
+            self.config = {}
+        
+        api.username = self.config.get("username")
+        api.token = self.config.get("token")
+        if api.username and api.token:
+            self.switch_to_repositories()
+    
+    def save_config(self):
+        with open(os.path.join(self, self.config_dir, CONFIG_FILENAME), "wt") as f:
+            json.dump(self.config, f)
     
     def on_quit_clicked(self, *args):
         Gtk.main_quit()
@@ -30,6 +52,9 @@ class Installer:
             self.api.login(username, password)
             page.set_widgets_sensitive(True)
             page.set_error()
+            self.config["username"] = self.api.username
+            self.config["token"] = self.api.token
+            self.save_config()
             self.switch_to_repositories()
         except ApiError as e:
             page.set_error(str(e))
