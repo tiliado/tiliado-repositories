b8b2d71128396f00ca8959f8c2ac1e960cbbf072
Jiří Janoušek <janousek.jiri@gmail.com>
2014-08-09 15:27:12 +0200

    Add setup.py
    Signed-off-by: Jiří Janoušek <janousek.jiri@gmail.com>

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+from distutils.core import setup
+setup(
+    name='tiliado-repositories',
+    version='0.1',
+    packages=['tiliadoweb', 'tiliadoweb.config'],
+    scripts=['tiliado-repositories'],
+    data_files=[("share/applications", ["tiliado-repositories.desktop"])],
+    )
