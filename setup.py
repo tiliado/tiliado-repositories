a77a57bb8eae0f72df089241881b906639cd3bd8
Jiří Janoušek <janousek.jiri@gmail.com>
2014-09-07 17:30:58 +0200

    Release 0.1.1
    Signed-off-by: Jiří Janoušek <janousek.jiri@gmail.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 setup(
     name='tiliado-repositories',
-    version='0.1',
+    version='0.1.1',
     packages=['tiliadoweb', 'tiliadoweb.config'],
     scripts=['tiliado-repositories'],
     data_files=[("share/applications", ["tiliado-repositories.desktop"])],
