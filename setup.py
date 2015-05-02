43f002be36475d1c4f0ebdd31a3b3a9781346d6a
Jiří Janoušek <janousek.jiri@gmail.com>
2015-05-02 10:04:06 +0200

    Release 0.3.5
    Signed-off-by: Jiří Janoušek <janousek.jiri@gmail.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 setup(
     name='tiliado-repositories',
-    version='0.3.4',
+    version='0.3.5',
     packages=['tiliadoweb', 'tiliadoweb.config'],
     scripts=['tiliado-repositories'],
     data_files=[("share/applications", ["tiliado-repositories.desktop"])],
