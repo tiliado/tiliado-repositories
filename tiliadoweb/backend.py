153b33c7dc63d987c185e3cc426f2411201fb83c
Jiří Janoušek <janousek.jiri@gmail.com>
2016-06-05 10:59:53 +0200

    Add and switch to dnf backend
    Issue: tiliado/tiliado-repositories#5
    
    Signed-off-by: Jiří Janoušek <janousek.jiri@gmail.com>

--- tiliadoweb/backend.py
+++ tiliadoweb/backend.py
@@ -147,6 +147,69 @@ class YumBackend(BaseBackend):
         argv = ["yum", "makecache", "fast", "-y"] + self.yum_opts
         exec_and_collects(argv, dry_run=self.dry_run)
 
+class DnfBackend(BaseBackend):
+    DEFAULT_KEY = "40554B8FA5FE6F6A"
+    
+    def __init__(self, *args, **kwargs):
+        BaseBackend.__init__(self, *args, **kwargs)
+        self.dnf_opts = []
+        #~ if not self.verify_ssl:
+            #~ log("Warning: ignoring SSL errors!")
+            #~ self.apt_opts.extend(('-o', 'Acquire::https::Verify-Peer=false', '-o', 'Acquire::https::Verify-Host=false'))
+    
+    def install_packages(self, packages):
+            argv = ["dnf", "install", "-y"] + self.dnf_opts + packages
+            exec_and_collects(argv, dry_run=self.dry_run)
+            
+    def remove_packages(self, packages):
+        argv = ["dnf", "remove", "-y"] + self.dnf_opts + packages
+        try:
+            exec_and_collects(argv, dry_run=self.dry_run)
+        except subprocess.CalledProcessError as e:
+            log(str(e))
+    
+    def add_repositories(self, protocol, server, username, token, products, dist_release, variants):
+        protocol = protocol or "https"
+        sources_dir = "/etc/yum.repos.d"
+        log("+ [makedirs '{}']".format(sources_dir))
+        if not self.dry_run:
+            try:
+                os.makedirs(sources_dir)
+            except OSError as e:
+                if e.errno != 17:
+                    raise e
+        
+        arch = os.uname()[4]
+        if arch != 'x86_64':
+            arch = 'i686'
+        
+        auth = "{}:{}@".format(username, token) if username and token else ""
+        for product in products:
+            buffer = []
+            for component in variants:
+                buffer.append('[{}-{}]'.format(product, component))
+                buffer.append('name={} repository, component {} ({} {})'.format(
+                    product, component, dist_release, arch))
+                buffer.append('baseurl={}://{}{}/{}/repository/rpm/{}/{}/{}/'.format(
+                    protocol, auth, server, product, dist_release, arch, component))
+                buffer.append('enabled=1')
+                buffer.append('gpgcheck=1')
+                buffer.append('repo_gpgcheck=1')
+                buffer.append('gpgkey=http://keyserver.ubuntu.com/pks/lookup?search=0x{}&op=get'.format(YumBackend.DEFAULT_KEY))
+                buffer.append('enabled_metadata=1')
+                buffer.append('')
+            
+            filename = "{}/tiliado-{}.repo".format(sources_dir, product)
+            write_file(filename, "\n".join(buffer), dry_run=self.dry_run)
+    
+    def add_key(self, key):
+        argv = ["rpm", "--import", "http://keyserver.ubuntu.com/pks/lookup?search=0x{}&op=get".format(key)]
+        exec_and_collects(argv, dry_run=self.dry_run)
+    
+    def update_db(self):
+        argv = ["dnf", "makecache", "--refresh", "-y"] + self.dnf_opts
+        exec_and_collects(argv, dry_run=self.dry_run)
+
 def install(server, protocol, project, distribution, release, variants, username=None, token=None,
     install=None, dry_run=False, no_verify_ssl=False, http_proxy=None, https_proxy=None, **kwd):
     if http_proxy is not None:
@@ -157,7 +220,7 @@ def install(server, protocol, project, distribution, release, variants, username
     if distribution in ("debian", "ubuntu"):
         backend = DebBackend(verify_ssl = not no_verify_ssl, dry_run=dry_run)
     elif distribution in ("fedora",):
-        backend = YumBackend(verify_ssl = not no_verify_ssl, dry_run=dry_run)
+        backend = DnfBackend(verify_ssl = not no_verify_ssl, dry_run=dry_run)
     else:
         log("Unsupported distribution: {}".format(distribution))
         sys.exit(2)
